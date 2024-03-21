9790f76054e6d5b7be47c91d71b274c87d01545a
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2024-03-21 18:00:21 -0500

    Added missing files of last commit

--- src/MathF.cs
+++ src/MathF.cs
@@ -19,4 +19,16 @@ public class MathInt {
 	public static int Floor(float num) {
 		return (int)MathF.Floor(num);
 	}
+
+	public static int Round(decimal num) {
+		return (int)Math.Round(num);
+	}
+
+	public static int Ceiling(decimal num) {
+		return (int)Math.Ceiling(num);
+	}
+
+	public static int Floor(decimal num) {
+		return (int)Math.Floor(num);
+	}
 }
