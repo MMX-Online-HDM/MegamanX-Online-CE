9554ea05c3a417c00d7085efbb6b1f77de9e52b9
OldGsU <119132322+GsU-Ninja@users.noreply.github.com>
2024-03-23 23:33:24 -0500

    Zero Block Vanilla behavior

--- src/Characters/Zero.cs
+++ src/Characters/Zero.cs
@@ -551,7 +551,7 @@ public class Zero : Character {
 		if (changedState) {
 			return true;
 		}
-		if (grounded && !isAttacking() && player.isZSaber() && (
+		if (charState is not Dash && grounded && !isAttacking() && player.isZSaber() && (
 				player.input.isHeld(Control.WeaponLeft, player) ||
 				player.input.isHeld(Control.WeaponRight, player)
 			) && (
@@ -559,6 +559,7 @@ public class Zero : Character {
 				player.input.isHeld(Control.Down, player)
 			)
 		) {
+			turnToInput(player.input, player);
 			changeState(new SwordBlock());
 			return true;
 		} else if (!player.isZBusterZero() && !isDashing && (
@@ -570,6 +571,7 @@ public class Zero : Character {
 		  ) {
 			if (!player.hasKnuckle()) {
 				if (grounded && !isAttacking()){
+				turnToInput(player.input, player);
 				changeState(new SwordBlock());}
 				return true;
 			} else if (parryCooldown == 0) {
