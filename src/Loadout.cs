db7d6a97a1a617abcc1880bc6aa9377560fd3858
OldGsU <119132322+GsU-Ninja@users.noreply.github.com>
2024-04-16 17:08:03 -0500

    Update Loadout.cs

--- src/Loadout.cs
+++ src/Loadout.cs
@@ -78,7 +78,6 @@ public class ZeroLoadout {
 			hyperMode = Helpers.randomRange(0, 2),
 			groundSpecial = Helpers.randomRange(0, 2),
 			airSpecial = Helpers.randomRange(0, 2),
-			melee = Helpers.randomRange(0, 2),
 		};
 	}
 
