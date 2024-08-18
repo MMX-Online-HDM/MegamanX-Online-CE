a6b2066528dbb5f79834627cf32ca7ce17257d7d
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2024-08-18 17:26:02 -0500

    Update DNACore.cs

--- src/Weapons/DNACore.cs
+++ src/Weapons/DNACore.cs
@@ -49,8 +49,10 @@ public class DNACore : AxlWeapon {
 		frozenCastle = character.player.frozenCastle;
 		speedDevil = character.player.speedDevil;
 		ultimateArmor = character is MegamanX { hasUltimateArmor: true };
-		if (charNum == 0) weapons = loadout.xLoadout.getWeaponsFromLoadout(character.player);
-		if (charNum == 1 && character is Zero zero) {
+		if (character is MegamanX) {
+			weapons = loadout.xLoadout.getWeaponsFromLoadout(character.player);
+		}
+		else if (character is Zero zero) {
 			rakuhouhaAmmo = zero.gigaAttack.ammo;
 			if (zero.hypermodeActive()) {
 				hyperMode = zero.hyperMode switch {
@@ -60,28 +62,43 @@ public class DNACore : AxlWeapon {
 				};
 			}
 		}
-		if (charNum == 2) weapons = loadout.vileLoadout.getWeaponsFromLoadout(false);
-		if (charNum == 3) {
+		else if (character is PunchyZero pzero) {
+			rakuhouhaAmmo = pzero.gigaAttack.ammo;
+			if (pzero.isBlack || pzero.isAwakened || pzero.isViral) {
+				hyperMode = pzero.hyperMode switch {
+					1 => DNACoreHyperMode.AwakenedZero,
+					2 => DNACoreHyperMode.NightmareZero,
+					_ => DNACoreHyperMode.BlackZero
+				};
+			}
+		}
+		else if (character is Axl) {
 			weapons = loadout.axlLoadout.getWeaponsFromLoadout();
 			if (weapons.Count > 0 && character.player.axlBulletType > 0) {
 				weapons[0] = character.player.getAxlBulletWeapon();
 			}
 		}
-		if (charNum == 4) {
+		else if (character is BaseSigma) {
 			rakuhouhaAmmo = character.player.sigmaAmmo;
 		}
 
-		// For any hyper modes added here, be sure to de-apply them if "preserve undisguise" is used in: axl.updateDisguisedAxl()
-		if (character.sprite.name.Contains("vilemk2")) hyperMode = DNACoreHyperMode.VileMK2;
-		else if (character.sprite.name.Contains("vilemk5")) hyperMode = DNACoreHyperMode.VileMK5;
-		else if (character is Axl axl && axl.isWhiteAxl()) hyperMode = DNACoreHyperMode.WhiteAxl;
-
+		// For any hyper modes added here.
+		// be sure to de-apply them if "preserve undisguise" is used in: axl.updateDisguisedAxl()
+		if (character.sprite.name.Contains("vilemk2")) {
+			hyperMode = DNACoreHyperMode.VileMK2;
+		} else if (character.sprite.name.Contains("vilemk5")) {
+			hyperMode = DNACoreHyperMode.VileMK5;
+		} else if (character is Axl axl && axl.isWhiteAxl()) {
+			hyperMode = DNACoreHyperMode.WhiteAxl;
+		}
 		rateOfFire = 1f;
 		index = (int)WeaponIds.DNACore;
 		weaponBarBaseIndex = 30 + charNum;
 		weaponBarIndex = weaponBarBaseIndex;
 		weaponSlotIndex = 30 + charNum;
-		if (charNum == 4) weaponSlotIndex = 65;
+		if (charNum == 4) {
+			weaponSlotIndex = 65;
+		}
 		if (charNum == (int)CharIds.BusterZero || charNum == (int)CharIds.PunchyZero) {
 			weaponBarBaseIndex = 31;
 		}
