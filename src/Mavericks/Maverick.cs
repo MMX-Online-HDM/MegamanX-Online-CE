68f920e16655dc68d9a0349b866708d04a47845d
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2024-09-09 01:51:12 -0500

    Update Maverick.cs

--- src/Mavericks/Maverick.cs
+++ src/Mavericks/Maverick.cs
@@ -415,11 +415,14 @@ public class Maverick : Actor, IDamagable {
 				target = Global.level.getClosestTarget(getCenterPos(), player.alliance, true, isRequesterAI: true);
 				doppler.ballType = 0;
 			}
-		} else if (!isSummonerCocoon && !isStrikerCocoon) {
-			target = Global.level.getClosestTarget(getCenterPos(), player.alliance, true, isRequesterAI: true);
-		} else {
+		} else if (isSummonerCocoon || isStrikerCocoon) {
 			target = mmc.getHealTarget();
-		}
+		} else if (!player.isPuppeteer() && !player.isTagTeam()) {
+			target = Global.level.getClosestTarget(
+				getCenterPos(), player.alliance, true, isRequesterAI: true,
+				aMaxDist: 300
+			);
+		} 
 
 		bool isAIState = (state is MIdle or MRun or MLand);
 		if (canFly) isAIState = isAIState || state is MFly;
