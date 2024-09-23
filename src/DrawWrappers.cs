c9e9921eef5ae467cca0c16cdd1339a7a1349c46
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2024-09-22 20:46:25 -0500

    Preliminary disabled new render

--- src/DrawWrappers.cs
+++ src/DrawWrappers.cs
@@ -1,222 +1,114 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using SFML.Graphics;
 using SFML.System;
 
 namespace MMXOnline;
 
-
-public interface IDrawableWrapper {
-	public (Drawable, RenderStates) GetDrawable(RenderTarget target, RenderStates states);
-}
-
-
-public class DrawableShape : IDrawableWrapper {
-	public Drawable drawable;
-
-	public DrawableShape(Drawable drawable) {
-		this.drawable = drawable;
-	}
-
-	public (Drawable, RenderStates) GetDrawable(RenderTarget target, RenderStates states) {
-		return (drawable, states);
-	}
-}
-
-public class DrawableFont : IDrawableWrapper {
+public class DrawableWrapper {
+	public List<ShaderWrapper>? shaders;
 	public Drawable drawable;
 	public Color color;
+	public int[]? size;
 
-	public DrawableFont(Drawable drawable, Color color) {
-		this.drawable = drawable;
-		this.color = color;
-	}
-
-	public (Drawable, RenderStates) GetDrawable(RenderTarget target, RenderStates states) {
-		return (drawable, states);
-	}
-}
-
-public class DrawableSprite : IDrawableWrapper {
-	public Drawable drawable;
-	public ShaderWrapper[] shaders;
-	public Color color;
-	public int[] size;
-
-	public DrawableSprite(
-		List<ShaderWrapper> shaders,
-		Drawable drawable,
-		Color color, int[] size
+	public DrawableWrapper(
+		List<ShaderWrapper> shaders, Drawable drawable,
+		Color? color = null, int[]? size = null
 	) {
-		shaders.RemoveAll(s => s == null);
-		this.shaders = shaders.ToArray();
+		shaders?.RemoveAll(s => s == null);
+		this.shaders = shaders;
 		this.drawable = drawable;
-		this.color = color;
+		this.color = color ?? Color.White;
 		this.size = size;
-	}
-
-	public (Drawable, RenderStates) GetDrawable(RenderTarget target, RenderStates states) {
-		return GetBaseDrawable(drawable, shaders, color, size, target, states);
-	}
-
-	public static (Drawable, RenderStates) GetBaseDrawable(
-		Drawable drawable, ShaderWrapper[] shaders,
-		Color color, int[] size,
-		RenderTarget target, RenderStates states
-	) {
-		Global.window.SetView(Global.view);
-		// No shaders.
-		if (shaders.Length == 0 || Options.main.disableShaders) {
-			return (drawable, states);
-		}
-		// Multi-shader.
-		else {
-			var sprite = drawable as SFML.Graphics.Sprite;
-			if (sprite == null) {
-				return (drawable, states);
-			}
-			RenderStates renderStates = new RenderStates(states);
-			Vector2f originalPosition = sprite.Position;
-			Vector2f originalOrigin = sprite.Origin;
-			float originalRotation = sprite.Rotation;
-			Vector2f originalScale = sprite.Scale;
-			Color originalColor = sprite.Color;
-			sprite.Position = new Vector2f(0, 0);
-			sprite.Origin = new Vector2f(0, 0);
-			sprite.Scale = new Vector2f(1, 1);
-			sprite.Rotation = 0;
-			sprite.Color = Color.White;
-			// Get textures.
-			int encodeKey = (size[0] * 397) ^ size[1];
-			// If something goes off.
-			if (!Global.renderTextures.ContainsKey(encodeKey)) {
-				Global.renderTextures[encodeKey] = (
-					new RenderTexture((uint)size[0], (uint)size[1]),
-					new RenderTexture((uint)size[0], (uint)size[1])
-				);
-			}
-			RenderTexture front;
-			RenderTexture back;
-			(front, back) = Global.renderTextures[encodeKey];
-			// Create a clear texture first.
-			back.Clear(new Color(0, 0, 0, 0));
-			back.Display();
-			back.Draw(sprite, renderStates);
-			back.Display();
-			// Iterate shaders.
-			for (int num = 0; num < shaders.Length; num++) {
-				// Clear image.
-				renderStates = new RenderStates(states);
-				front.Clear(new Color(0, 0, 0, 0));
-				front.Display();
-				// Apply shader and draw.
-				renderStates.Shader = shaders[num].getShader();
-				front.Draw(
-					new SFML.Graphics.Sprite(back.Texture), renderStates
-				);
-				// Swap.
-				(
-					front,
-					back
-				) = (
-					back,
-					front
-				);
-			}
-			// Final result.
-			var finalSprite = new SFML.Graphics.Sprite(back.Texture);
-			finalSprite.Position = originalPosition;
-			finalSprite.Origin = originalOrigin;
-			finalSprite.Scale = originalScale;
-			finalSprite.Rotation = originalRotation;
-			finalSprite.Color = originalColor;
-
-			renderStates = new RenderStates(states);
-			return (finalSprite, renderStates);
-		}
-	}
-}
-
-public class DrawableCompositeSprite : IDrawableWrapper {
-	public DrawableSprite[] subSprites;
-	public ShaderWrapper[] shaders;
-	public int[] size;
-	public Vector2f pos;
-	public float rotation;
-	public Vector2f scale;
-	public Vector2f origin;
-	public Color color;
-
-	public DrawableCompositeSprite(
-		DrawableSprite[] subSprites,
-		List<ShaderWrapper> shaders,
-		int[] size,
-		Vector2f pos,
-		float rotation,
-		Vector2f scale,
-		Vector2f origin,
-		Color color
-	) {
-		shaders.RemoveAll(s => s == null);
-		this.shaders = shaders.ToArray();
-		this.subSprites = subSprites;
-		this.size = size;
-		this.pos = pos;
-		this.rotation = rotation;
-		this.scale = scale;
-		this.origin = origin;
-		this.color = color;
-	}
-
-	public (Drawable, RenderStates) GetDrawable(RenderTarget target, RenderStates states) {
-		Drawable[] sprites = new Drawable[subSprites.Length];
-
-		for (int i = 0; i < subSprites.Length; i++) {
-			sprites[i] = subSprites[i].GetDrawable(target, states).Item1;
-		}
-		return ComposeSprite(sprites, target, states);
-	}
-
-	public (Drawable, RenderStates) ComposeSprite(Drawable[] spriteParts, RenderTarget target, RenderStates states) {
-		// Get textures.
-		int encodeKey = (size[0] * 397) ^ size[1];
-		// If something goes off.
-		if (!Global.renderTextures.ContainsKey(encodeKey)) {
-			Global.renderTextures[encodeKey] = (
-				new RenderTexture((uint)size[0], (uint)size[1]),
-				new RenderTexture((uint)size[0], (uint)size[1])
-			);
-		}
-		RenderTexture renderTexture = Global.renderTextures[encodeKey].Item1;
-		RenderStates renderStates = new RenderStates(states);
-		// Create a clear texture first.
-		renderTexture.Clear(new Color(0, 0, 0, 0));
-		// Start to draw sprites one on top of another without clearing.
-		for (int i = 0; i < spriteParts.Length; i++) {
-			renderTexture.Display();
-			renderTexture.Draw(spriteParts[i], renderStates);
-		}
-		// Final result.
-		var finalSprite = new SFML.Graphics.Sprite(renderTexture.Texture);
-		finalSprite.Position = pos;
-		finalSprite.Origin = origin;
-		finalSprite.Scale = scale;
-		finalSprite.Rotation = rotation;
-		finalSprite.Color = color;
-		// Draw the composite sprite normally.
-		return DrawableSprite.GetBaseDrawable(finalSprite, shaders, color, size, target, renderStates);
 	}
 }
 
 public class DrawLayer : Transformable, Drawable {
-	public List<IDrawableWrapper> oneOffs = new List<IDrawableWrapper>();
+	public List<DrawableWrapper> oneOffs = new List<DrawableWrapper>();
+
+	public DrawLayer() {
+
+	}
 
 	public void Draw(RenderTarget target, RenderStates states) {
-		foreach (IDrawableWrapper drawableWrapper in oneOffs) {
-			Drawable localDrawable;
-			RenderStates localState;
-			(localDrawable, localState) = drawableWrapper.GetDrawable(target, states);
-			target.Draw(localDrawable, localState);
+		for (int i = 0; i < oneOffs.Count; i++) {
+			var oneOff = oneOffs[i];
+			Global.window.SetView(Global.view);
+			// No shaders.
+			if (oneOff.shaders == null || oneOff.shaders.Count == 0) {
+				target.Draw(oneOff.drawable);
+			}
+			// One shader.
+			else if (oneOff.shaders.Count == 1 && oneOff.color == Color.White) {
+				RenderStates renderStates = new RenderStates(states);
+				renderStates.Shader = oneOff.shaders[0].getShader();
+				target.Draw(oneOff.drawable, renderStates);
+			}
+			// Multi-shader.
+			else {
+				var sprite = oneOff.drawable as SFML.Graphics.Sprite;
+				if (sprite == null) {
+					continue;
+				}
+				RenderStates renderStates = new RenderStates(states);
+				Vector2f originalPosition = sprite.Position;
+				Vector2f originalOrigin = sprite.Origin;
+				float originalRotation = sprite.Rotation;
+				Vector2f originalScale = sprite.Scale;
+				Color originalColor = sprite.Color;
+				sprite.Position = new Vector2f(0, 0);
+				sprite.Origin = new Vector2f(0, 0);
+				sprite.Scale = new Vector2f(1, 1);
+				sprite.Rotation = 0;
+				sprite.Color = Color.White;
+				// Get textures.
+				int encodeKey = (oneOff.size[0] * 397) ^ oneOff.size[1];
+				// If something goes off.
+				if (!Global.renderTextures.ContainsKey(encodeKey)) {
+					Global.renderTextures[encodeKey] = (
+						new RenderTexture((uint)oneOff.size[0], (uint)oneOff.size[1]),
+						new RenderTexture((uint)oneOff.size[0], (uint)oneOff.size[1])
+					);
+				}
+				RenderTexture front;
+				RenderTexture back;
+				(front, back) = Global.renderTextures[encodeKey];
+				// Create a clear texture first.
+				back.Clear(new Color(0, 0, 0, 0));
+				back.Display();
+				renderStates.Shader = oneOff.shaders[0].getShader();
+				back.Draw(sprite, renderStates);
+				// Iterate shaders.
+				for (int num = 1; num < oneOff.shaders.Count; num++) {
+					// Clear image.
+					renderStates = new RenderStates(states);
+					front.Clear(new Color(0, 0, 0, 0));
+					front.Display();
+					// Apply shader and draw.
+					renderStates.Shader = oneOff.shaders[num].getShader();
+					front.Draw(
+						new SFML.Graphics.Sprite(back.Texture), renderStates
+					);
+					// Swap.
+					(
+						front,
+						back
+					) = (
+						back,
+						front
+					);
+				}
+				// Final result.
+				var finalSprite = new SFML.Graphics.Sprite(back.Texture);
+				finalSprite.Position = originalPosition;
+				finalSprite.Origin = originalOrigin;
+				finalSprite.Scale = originalScale;
+				finalSprite.Rotation = originalRotation;
+				finalSprite.Color = originalColor;
+
+				renderStates = new RenderStates(states);
+				target.Draw(finalSprite, renderStates);
+			}
 		}
 	}
 }
@@ -245,7 +137,7 @@ public partial class DrawWrappers {
 
 		if (isWorldPos) {
 			DrawLayer drawLayer = getDrawLayer(depth);
-			drawLayer.oneOffs.Add(new DrawableShape(line));
+			drawLayer.oneOffs.Add(new DrawableWrapper(null, line));
 		} else {
 			drawToHUD(line);
 		}
@@ -285,7 +177,7 @@ public partial class DrawWrappers {
 
 		if (isWorldPos) {
 			DrawLayer drawLayer = getDrawLayer(depth);
-			drawLayer.oneOffs.Add(new DrawableShape(circle));
+			drawLayer.oneOffs.Add(new DrawableWrapper(null, circle));
 		} else {
 			drawToHUD(circle);
 		}
@@ -305,7 +197,7 @@ public partial class DrawWrappers {
 
 		if (isWorldPos) {
 			DrawLayer drawLayer = getDrawLayer(depth);
-			drawLayer.oneOffs.Add(new DrawableShape(pixelSprite));
+			drawLayer.oneOffs.Add(new DrawableWrapper(null, pixelSprite));
 		} else {
 			drawToHUD(pixelSprite);
 		}
@@ -335,7 +227,7 @@ public partial class DrawWrappers {
 
 		if (isWorldPos) {
 			DrawLayer drawLayer = getDrawLayer(depth);
-			drawLayer.oneOffs.Add(new DrawableShape(rect));
+			drawLayer.oneOffs.Add(new DrawableWrapper(null, rect));
 		} else {
 			drawToHUD(rect);
 		}
@@ -371,7 +263,7 @@ public partial class DrawWrappers {
 		}
 		if (isWorldPos) {
 			DrawLayer drawLayer = getDrawLayer(depth);
-			drawLayer.oneOffs.Add(new DrawableShape(shape));
+			drawLayer.oneOffs.Add(new DrawableWrapper(null, shape));
 		} else {
 			drawToHUD(shape);
 		}
@@ -383,12 +275,9 @@ public partial class DrawWrappers {
 		float cx = 0, float cy = 0,
 		float xScale = 1, float yScale = 1,
 		float angle = 0, float alpha = 1,
-		List<ShaderWrapper> shaders = null!, bool isWorldPos = true
+		List<ShaderWrapper> shaders = null, bool isWorldPos = true
 	) {
 		if (texture == null) return;
-		if (shaders == null) {
-			shaders = new();
-		}
 
 		if (isWorldPos && Options.main.enablePostProcessing) {
 			dx -= Global.level.camX;
@@ -408,55 +297,10 @@ public partial class DrawWrappers {
 
 		if (isWorldPos) {
 			DrawLayer drawLayer = getDrawLayer(depth);
-			drawLayer.oneOffs.Add(new DrawableSprite(shaders, sprite, sprite.Color, [(int)sw, (int)sh]));
+			drawLayer.oneOffs.Add(new DrawableWrapper(shaders, sprite, sprite.Color, [(int)sw, (int)sh]));
 		} else {
 			drawToHUD(sprite);
 		}
-	}
-
-	public static void DrawCompositeTexture(
-		Texture[] texture, float sx,
-		float sy, float sw, float sh,
-		float dx, float dy, long depth,
-		float cx = 0, float cy = 0,
-		float xScale = 1, float yScale = 1,
-		float angle = 0, float alpha = 1,
-		List<ShaderWrapper> shaders = null!, bool isWorldPos = true
-	) {
-		if (shaders == null) {
-			shaders = new();
-		}
-		if (isWorldPos && Options.main.enablePostProcessing) {
-			dx -= Global.level.camX;
-			dy -= Global.level.camY;
-		}
-		dx = MathF.Round(dx);
-		dy = MathF.Round(dy);
-		cx = MathF.Floor(cx);
-		cy = MathF.Floor(cy);
-		int[] size = [(int)sw, (int)sh];
-
-		var drawables = new DrawableSprite[texture.Length];
-		for (int i = 0; i < texture.Length; i++) {
-			var sprite = new SFML.Graphics.Sprite(texture[i], new IntRect((int)sx, (int)sy, (int)sw, (int)sh));
-			sprite.Position = new Vector2f(0, 0);
-			sprite.Origin = new Vector2f(0, 0);
-			sprite.Scale = new Vector2f(1, 1);
-			sprite.Rotation = 0;
-			sprite.Color = Color.White;
-			drawables[i] = new DrawableSprite([], sprite, sprite.Color, size);
-		}
-
-		DrawLayer drawLayer = getDrawLayer(depth);
-		drawLayer.oneOffs.Add(new DrawableCompositeSprite(
-			drawables, shaders,
-			size,
-			new Vector2f(dx, dy),
-			angle,
-			new Vector2f(xScale, yScale),
-			new Vector2f(cx, cy),
-			new Color(255, 255, 255, (byte)(int)(alpha * 255))
-		));
 	}
 
 	public static void DrawMapTiles(Texture[,] textureMDA, float x, float y, RenderTexture screenRenderTexture = null, ShaderWrapper shaderWrapper = null) {
