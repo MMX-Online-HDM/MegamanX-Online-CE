3ba9d3286ee81449e5312ce87e4c67f1c6fe73ba
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2024-11-28 14:22:39 -0500

    Update RenderEffect.cs

--- src/RenderEffect.cs
+++ src/RenderEffect.cs
@@ -37,6 +37,6 @@ public class RenderEffect {
 	}
 
 	public bool isFlashing() {
-		return time % flashTime * 2 < flashTime;
+		return Global.level.nonSkippedframeCount % (flashTime * 2) < flashTime;
 	}
 }
