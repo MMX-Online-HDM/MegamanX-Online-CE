626aa8627d62d96d9740ae32ec9a897f78fa6179
OldGsU <oldgsu18@gmail.com>
2025-08-04 21:26:51 -0500

    ai

--- src/Sigma/CmdSigma.cs
+++ src/Sigma/CmdSigma.cs
@@ -266,7 +266,8 @@ public class CmdSigma : BaseSigma {
 				if (weapon is MaverickWeapon mw && mw.maverick != null) {
 					mw.maverick.changeState(new MExit(mw.maverick.pos, true), true);
 				}
-			}	
+			}
 		}
+		base.aiUpdate(target);
 	}
 }
