dec8fa8567f03e5375c24a93f5e954d2eb701f7c
OldGsU <oldgsu18@gmail.com>
2025-08-30 01:20:05 -0500

    Update RaySplasher.cs

--- src/X/Weapons/RaySplasher.cs
+++ src/X/Weapons/RaySplasher.cs
@@ -42,6 +42,10 @@ public class RaySplasher : Weapon {
 			if (mmx.shootingRaySplasher != null) {
 				mmx.raySplasherCooldown += Global.speedMul;
 				if (mmx.raySplasherCooldown > 1) {
+					//Stop when invincible
+					if (mmx.charState.invincible) return;
+					if (mmx.invulnTime > 0) return;
+
 					if (mmx.raySplasherCooldown >= 4) {
 						addAmmo(-0.15f, mmx.player);
 						mmx.raySplasherCooldown = 1;
@@ -55,7 +59,7 @@ public class RaySplasher : Weapon {
 					mmx.shootAnimTime = 10;
 					mmx.raySplasherCooldown2 += Global.speedMul;
 				}
-				if (mmx.raySplasherCooldown2 >= 76) {
+				if (mmx.raySplasherCooldown2 >= 66) {
 					mmx.shootingRaySplasher = null;
 					mmx.raySplasherCooldown2 = 0;
 				}
